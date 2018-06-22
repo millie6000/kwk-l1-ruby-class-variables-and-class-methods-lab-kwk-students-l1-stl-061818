62a3cdcc22ba03541b8a0f7d8f055b779b4514d8
millie6000 <alohaturtle2003@gmail.com>
2018-06-22 19:37:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,23 +6,24 @@ def initialize(jen, artist, name)
   @name = name 
 end 
 
-end 
-def gets_jenje
-  @jenje 
-end 
+attr_accessor
+# end 
+# def gets_jenje
+#   @jenje 
+# end 
 
-def gets_artist
-  @artist
-end
-def gets_name
-  @name 
-end 
+# def gets_artist
+#   @artist
+# end
+# def gets_name
+#   @name 
+# end 
 
 
-end 
+# end 
 
-jon_song = Song.new("hippity hoppity","jon mendes","jon's way")
-puts jon_song.gets_jenje
-puts "wow what a great choice"
-puts jon_song.gets_name
-puts jon_song.gets_artist
+# jon_song = Song.new("hippity hoppity","jon mendes","jon's way")
+# puts jon_song.gets_jenje
+# puts "wow what a great choice"
+# puts jon_song.gets_name
+# puts jon_song.gets_artist
