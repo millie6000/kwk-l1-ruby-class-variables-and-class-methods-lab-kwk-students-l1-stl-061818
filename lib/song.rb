fb30930ca382c67b04b8832cea1ccdd221c739da
millie6000 <alohaturtle2003@gmail.com>
2018-06-22 19:34:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,28 @@
 class Song 
+
+def initialize(jen, artist, name)
+  @jenje = jenje
+  @artist = artist
+  @name = name 
+end 
+
+end 
+def gets_jenje
+  @jenje 
+end 
+
+def gets_artist
+  @artist
+end
+def gets_name
+  @name 
+end 
+
+
+end 
+
+jon_song = Song.new("hippity hoppity","jon mendes","jon's way")
+puts jon_song.gets_jenje
+puts "wow what a great choice"
+puts jon_song.gets_name
+puts jon_song.gets_artist
