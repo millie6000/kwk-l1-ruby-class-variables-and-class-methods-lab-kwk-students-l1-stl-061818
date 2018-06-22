5930b904a5ea71a4ce8913c9a2850aa3595c7ab8
millie6000 <alohaturtle2003@gmail.com>
2018-06-22 20:30:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,14 @@ end
 
 end 
 
+
+
+def get_count_other_way
+  @@count 
+end
+
+end
+
 jon_song = Song.new("hippity hoppity","jon mendez","jon's way")
 big_song = Song.new("l","n","u")
 puts "this is the first method"
