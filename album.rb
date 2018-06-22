c012b81b038e05be0ea1edd1c0a1031535be694e
millie6000 <alohaturtle2003@gmail.com>
2018-06-22 18:50:08 +0000

    automatically backed up by learn

--- album.rb
+++ album.rb
@@ -1,5 +1,17 @@
 class Album
   
-  
-
+ def initialize(name,date)  # spell correctly initialize 
+@name=name
+@date=date
 end
+
+def name_of_album
+  @name
+end
+
+def date_of_album
+  @date 
+end
+end
+
+shawn_mendes=
