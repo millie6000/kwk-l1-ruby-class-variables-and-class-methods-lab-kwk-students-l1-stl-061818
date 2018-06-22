fd732272897e8937b01afcf8de77bb2317788746
millie6000 <alohaturtle2003@gmail.com>
2018-06-22 19:07:08 +0000

    automatically backed up by learn

--- album.rb
+++ album.rb
@@ -1,10 +1,16 @@
 class Album
+
+  def initialize
   
  def initialize(name,date)  # spell correctly initialize 
 @name=name
 @date=date
+@@album_coun=@@album_count+1 
 end
 
+def self.count 
+  @@album_count
+  
 def name_of_album
   @name
 end
@@ -15,5 +21,9 @@ end
 end
 
 shawn_mendes=Album.new("shawn_mendes","2018")
+taylor_swift=Album.new("reputation","2017")
+drake=Album.new("scorpian","2018")
 
 puts "album was made #{shawn_mendes.date_of_album}"
+#puts " the album was made #{shawn_mendes. date_of_album"
+puts "#{Album.coun}"
